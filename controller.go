3017b76cb14e4e3c782abeae66075c46c4c611a8
Michael Whatcott <mdwhatcott@gmail.com>
2017-08-10 10:54:42 -0600

    Defined interfaces.

--- controller.go
+++ controller.go
@@ -1,1 +1,21 @@
 package tomato
+
+import (
+	"io"
+	"time"
+)
+
+type TimeCounter interface {
+	Countdown(duration time.Duration) chan time.Time
+}
+
+type OS interface {
+	FocusApp(name string)
+	LockScreen()
+}
+
+type Controller struct {
+	terminal io.ReadWriter
+	timer    TimeCounter
+	os       OS
+}
