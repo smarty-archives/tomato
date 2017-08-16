e64ba1277fdde8140d8f9676cac7d2098e378fb4
Michael Whatcott <mdwhatcott@gmail.com>
2017-08-16 11:53:09 -0600

    No need to panic because of build constraint from previous commit.

--- tomato/main.go
+++ tomato/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"log"
 	"os"
-	"runtime"
 	"time"
 
 	"github.com/smartystreets/tomato"
@@ -14,10 +13,6 @@ const maxTomatoesPerDay = 16
 func main() {
 	log.SetFlags(log.Ltime)
 	log.SetOutput(os.Stdout)
-
-	if runtime.GOOS != "darwin" {
-		log.Fatal("This application only supports macos.")
-	}
 
 	tomato.NewController(os.Stdin, new(tomato.MacOS), maxTomatoesPerDay, time.Minute).Run()
 }
