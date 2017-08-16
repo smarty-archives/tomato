9b95b4edc3bb75b4fc2c7f48447282ca650b6785
Michael Whatcott <mdwhatcott@gmail.com>
2017-08-16 11:52:17 -0600

    Moved macos-specific code to a separate file with a build constraint.

--- os.go
+++ os.go
@@ -1,36 +1,9 @@
 package tomato
 
-import (
-	"fmt"
-	"log"
-	"os/exec"
-	"time"
-)
+import "time"
 
 type GenericOS struct{}
 
 func (this *GenericOS) Sleep(duration time.Duration) {
 	time.Sleep(duration)
-}
-
-type MacOS struct{ *GenericOS }
-
-func (this *MacOS) Notify(message string) {
-	notification := fmt.Sprintf("display notification \"%s\" with title \"tomato Timer\"", message)
-	this.execute(exec.Command("osascript", "-e", notification))
-}
-
-func (this *MacOS) FocusApp(name string) {
-	this.execute(exec.Command("open", "-a", name))
-}
-
-func (this *MacOS) LockScreen() {
-	const screenSaver = "/System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app"
-	this.FocusApp(screenSaver)
-}
-
-func (this *MacOS) execute(command *exec.Cmd) {
-	if output, err := command.CombinedOutput(); err != nil {
-		log.Println("[WARN]", string(output), err)
-	}
 }
---
+++ os_darwin.go
@@ -0,0 +1,29 @@
+package tomato
+
+import (
+	"fmt"
+	"log"
+	"os/exec"
+)
+
+type MacOS struct{ *GenericOS }
+
+func (this *MacOS) Notify(message string) {
+	notification := fmt.Sprintf("display notification \"%s\" with title \"tomato Timer\"", message)
+	this.execute(exec.Command("osascript", "-e", notification))
+}
+
+func (this *MacOS) FocusApp(name string) {
+	this.execute(exec.Command("open", "-a", name))
+}
+
+func (this *MacOS) LockScreen() {
+	const screenSaver = "/System/Library/Frameworks/ScreenSaver.framework/Versions/A/Resources/ScreenSaverEngine.app"
+	this.FocusApp(screenSaver)
+}
+
+func (this *MacOS) execute(command *exec.Cmd) {
+	if output, err := command.CombinedOutput(); err != nil {
+		log.Println("[WARN]", string(output), err)
+	}
+}
